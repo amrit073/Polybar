4fbe40de56ea77f7d76d4c6d331bc65adb4f5b57
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2022-10-19 19:38:56 +0545

    gruvbox

--- scripts/music.sh
+++ scripts/music.sh
@@ -21,7 +21,6 @@ zscroll --before-text "" --delay 0.3 \
     --match-command "playerctl status" \
     --match-text "Playing" "--before-text ' '" \
     --match-text "Paused" "--before-text ' ' --scroll 0" \
-    --match-text "No players found" "--before-text '' --scroll 0" \
     --update-check true "playerctl metadata title" &
 
 wait
