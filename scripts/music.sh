d5e768579afcc07347a60c694a8a2853b324d7eb
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2023-09-03 12:39:24 +0545

    no error message in player

--- scripts/music.sh
+++ scripts/music.sh
@@ -21,7 +21,7 @@ zscroll --before-text "" --delay 0.3 \
     --match-command "playerctl status" \
     --match-text "Playing" "--before-text ' '" \
     --match-text "Paused" "--before-text ' ' --scroll 0" \
-    --update-check true "playerctl metadata title" &
+    --update-check true "playerctl metadata title --no-messages" &
 
 wait
 
